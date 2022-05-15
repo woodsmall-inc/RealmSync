cfa731582cd5dd6adbc2a96f28719573b09530c7
kobayashi <kobayashi@woodsmall.co.jp>
2022-05-15 14:07:21 +0900

    最初のソース

---
+++ RealmSync/RealmSync/Pods/Target Support Files/Pods-RealmSync-RealmSyncUITests/Pods-RealmSync-RealmSyncUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealmSync_RealmSyncUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RealmSync_RealmSyncUITests
+@end
---
+++ RealmSync/RealmSync/Pods/Target Support Files/Pods-RealmSyncTests/Pods-RealmSyncTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealmSyncTests : NSObject
+@end
+@implementation PodsDummy_Pods_RealmSyncTests
+@end
